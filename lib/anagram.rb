3532eb280779025e9dc2b66ad3b1332b880f6e97
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-16 05:35:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,6 +2,6 @@ class Anagram
   attr_accessor :diaper
 
   def initialize(diaper)
-    diaper.match(%w(hello world zombies pants dipper)) => diaper
+    diaper.match(%w(hello world zombies pants dipper)) => ["diaper"]
   end
 end 
