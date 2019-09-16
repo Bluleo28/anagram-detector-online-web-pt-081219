8cfc9f3b43848debc8c3a79966cc08feafd78fb3
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-16 05:47:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,4 +4,13 @@ class Anagram
   def initialize(diaper)
       diaper.match(%w(hello world zombies pants dipper))
   end
+  def detector(ba)
+    ba = detector.match (["ab", "abc", "bac"])
+  end 
+  def detector(listen)
+      listen = detector.match(%w(enlists google inlets banana))
+  end 
+  def detector(allergy)
+      allergy = detector.match(%w(gallery ballerina regally clergy largely leading))
+    end
 end
