c09980376820daddf251f26e2de00a483e5f17de
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-16 05:55:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,13 +4,15 @@ class Anagram
   def initialize(word)
       @word = word
   end
-  def detector(ba)
-      ba == (["ab", "abc", "bac"])
+  def match(array_of_words)
+    array_of_words.select do |element|
+    ([@word.split("").sort) == (element.split("").sort)
   end
-  def detector(listen)
-      listen == (%w(enlists google inlets banana))
-  end
-  def detector(allergy)
-      allergy == (%w(gallery ballerina regally clergy largely leading))
-    end
 end
+#   def detector(listen)
+#       listen == (%w(enlists google inlets banana))
+#   end
+#   def detector(allergy)
+#       allergy == (%w(gallery ballerina regally clergy largely leading))
+#     end
+# end
